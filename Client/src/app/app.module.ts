8743b3e0056a0b3a00dc3fb864a6c462cd2530ad
Pavlo Boliubash <5avel@hotmail.com>
2019-02-25 08:48:12 +0200

    Create bar chart, directory structure, update styles

--- Client/src/app/app.module.ts
+++ Client/src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { ChartsModule } from 'ng2-charts';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -26,7 +27,8 @@ import { PieChartComponent } from './charts/pie-chart/pie-chart.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    ChartsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
